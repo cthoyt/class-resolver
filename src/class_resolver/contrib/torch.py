0730638bb043ec6586645e8d8a5ad2dcc9a1b0ce
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-02-06 12:52:13 +0100

    Fix optimizer resolution

--- src/class_resolver/contrib/torch.py
+++ src/class_resolver/contrib/torch.py
@@ -2,11 +2,32 @@
 
 """Class resolvers for PyTorch."""
 
+from torch import nn
 from torch.optim import Adam, Optimizer
 
 from ..api import Resolver
 
-optimizer_resolver = Resolver.from_subclasses(Optimizer, default=Adam)
+__all__ = [
+    "optimizer_resolver",
+    "activation_resolver",
+]
+
+optimizer_resolver = Resolver.from_subclasses(Optimizer, default=Adam, suffix="")
+
+activation_resolver = Resolver(
+    classes=(
+        nn.LeakyReLU,
+        nn.PReLU,
+        nn.ReLU,
+        nn.Softplus,
+        nn.Sigmoid,
+        nn.Tanh,
+    ),
+    base=nn.Module,
+    suffix="",
+    default=nn.ReLU,
+)
 
 if __name__ == '__main__':
+    print(activation_resolver.options)
     print(optimizer_resolver.options)
