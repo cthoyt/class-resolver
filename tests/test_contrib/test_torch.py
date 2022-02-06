0cd1009946d58310282eb1ec4485e58d17816a23
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-02-06 13:17:00 +0100

    Update

--- tests/test_contrib/test_torch.py
+++ tests/test_contrib/test_torch.py
@@ -16,22 +16,28 @@ class TestTorch(unittest.TestCase):
 
     def test_activation(self):
         """Tests for the activation resolver."""
-        from class_resolver.contrib.torch import activation_resolver
         from torch.nn import ReLU, Softplus
+
+        from class_resolver.contrib.torch import activation_resolver
+
         self.assertEqual(Softplus, activation_resolver.lookup("softplus"))
         self.assertEqual(ReLU, activation_resolver.lookup("relu"))
         self.assertEqual(ReLU, activation_resolver.lookup())
 
     def test_optimizer(self):
         """Tests for the optimizer resolver."""
+        from torch.optim import Adagrad, Adam
+
         from class_resolver.contrib.torch import optimizer_resolver
-        from torch.optim import Adam, Adagrad
+
         self.assertEqual(Adagrad, optimizer_resolver.lookup("adagrad"))
         self.assertEqual(Adam, optimizer_resolver.lookup("adam"))
         self.assertEqual(Adam, optimizer_resolver.lookup())
 
     def test_initializer(self):
         """Tests for the initializer function resolver."""
-        from class_resolver.contrib.torch import initializer_resolver
         from torch.nn.init import xavier_normal_
+
+        from class_resolver.contrib.torch import initializer_resolver
+
         self.assertEqual(xavier_normal_, initializer_resolver.lookup("xavier_normalize_"))
