86b37aff38970e2b010c13bce2fb39569f7bb508
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-02-09 11:26:24 +0100

    Update imports

--- src/class_resolver/contrib/torch.py
+++ src/class_resolver/contrib/torch.py
@@ -5,8 +5,8 @@
 from torch import nn
 from torch.nn import init
 from torch.nn.modules import activation
-from torch.optim import Adam, Optimizer, ReduceLROnPlateau
-from torch.optim.lr_scheduler import ExponentialLR, _LRScheduler
+from torch.optim import Adam, Optimizer
+from torch.optim.lr_scheduler import ExponentialLR, ReduceLROnPlateau, _LRScheduler
 
 from ..api import ClassResolver
 from ..func import FunctionResolver
