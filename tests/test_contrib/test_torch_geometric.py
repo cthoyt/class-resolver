b558166040520a95e4cf5402466b322d49df69f2
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-02-19 21:58:07 +0100

    Add more testing

--- tests/test_contrib/test_torch_geometric.py
+++ tests/test_contrib/test_torch_geometric.py
@@ -6,8 +6,8 @@ import unittest
 
 try:
     import torch_geometric
-except torch_geometric:  # pragma: no cover
-    torch = None  # pragma: no cover
+except ImportError:  # pragma: no cover
+    torch_geometric = None  # pragma: no cover
 
 
 @unittest.skipUnless(
@@ -24,3 +24,20 @@ class TestTorch(unittest.TestCase):
 
         self.assertEqual(SimpleConv, message_passing_resolver.lookup("simple"))
         self.assertEqual(SimpleConv, message_passing_resolver.lookup(None))
+
+    def test_aggregation(self):
+        """Test the aggregation resolver."""
+        import torch
+
+        from class_resolver.contrib.torch_geometric import aggregation_resolver
+
+        # Feature matrix holding 1000 elements with 64 features each:
+        x = torch.randn(1000, 64)
+
+        # Randomly assign elements to 100 sets:
+        index = torch.randint(0, 100, (1000,))
+
+        for cls in aggregation_resolver:
+            aggr = cls()
+            output = aggr(x, index)
+            self.assertEqual((100, 64), tuple(output.shape))
