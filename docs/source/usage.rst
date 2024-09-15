b70b706d7a4d9c0cf29013c051170d33f3057f43
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-09-15 23:14:53 +0200

    Add example for showing during build

--- docs/source/usage.rst
+++ docs/source/usage.rst
@@ -2,3 +2,6 @@ Usage
 =====
 .. automodapi:: class_resolver
     :inherited-members:
+
+
+.. autofunction:: class_resolver.ex.f
---
+++ src/class_resolver/ex.py
@@ -0,0 +1,37 @@
+"""Example.
+
+Delete this before merging
+"""
+
+from typing import Any, Union
+
+from torch import Tensor, nn
+
+from class_resolver import document_resolver
+from class_resolver.contrib.torch import activation_resolver, aggregation_resolver
+
+__all__ = ["f"]
+
+
+@document_resolver("activation", resolver_name="class_resolver.contrib.torch.activation_resolver")
+@document_resolver("aggregation", resolver_name="class_resolver.contrib.torch.aggregation_resolver")
+def f(
+    tensor: Tensor,
+    activation: Union[None, str, type[nn.Module], nn.Module],
+    activation_kwargs: dict[str, Any] | None,
+    aggregation: Union[None, str, type[nn.Module], nn.Module],
+    aggregation_kwargs: dict[str, Any] | None,
+):
+    """Apply an activation then aggregation.
+
+    :param tensor: An input tensor
+    :param activation: An activation function (stateful)
+    :param activation_kwargs: Keyword arguments for activation function
+    :param aggregation: An aggregation function (stateful)
+    :param aggregation_kwargs: Keyword arguments for aggregation function
+
+    :return: An aggregation
+    """
+    _activation = activation_resolver.make(activation, activation_kwargs)
+    _aggregation = aggregation_resolver.make(aggregation, aggregation_kwargs)
+    return _aggregation(_activation(tensor))
